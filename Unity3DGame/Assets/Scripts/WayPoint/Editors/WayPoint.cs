07f82225b708bf5bb6cba281e45b80c73b42e8a4
admin <chj09255@gmail.com>
2023-06-08 10:22:03 +0900

    bu

---
+++ Unity3DGame/Assets/Scripts/WayPoint/Editors/WayPoint.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+    private GameObject parent;
+
+    void Start()
+    {
+        parent = GameObject.Find("ParentObject");
+    }
+
+    void Update()
+    {
+        for (int i =0;i<parent.transform.childCount;++i)
+        {
+            Node node = parent.transform.GetChild(i).GetComponent<Node>();
+
+            Debug.DrawLine(node.transform.position, node.Next.transform.position);
+        }
+    }
+}
